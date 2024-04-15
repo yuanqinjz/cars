ef533a3082939d20ab1bbdbcb2eb5c67f8d932a2
yuanqinjz <59837248+yuanqinjz@users.noreply.github.com>
2024-04-15 09:27:15 -0500

    Add files via upload

---
+++ carui/src/App.tsx
@@ -0,0 +1,27 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import CssBaseline from '@mui/material/CssBaseline';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+function App() {
+ return (
+    <Container maxWidth="xl">
+        <CssBaseline />
+        <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6">
+                    Car Shop
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <QueryClientProvider client={queryClient}>
+        </QueryClientProvider>
+    </Container>
+ );
+}
+
+export default App
